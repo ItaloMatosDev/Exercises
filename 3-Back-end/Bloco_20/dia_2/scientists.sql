f29589ad701edf6f900b4d938bf471ebd08036c3
Italo <italomatos764@gmail.com>
2021-07-20 15:02:59 -0400

    Table scientists

---
+++ 3-Back-end/Bloco_20/dia_2/scientists.sql
@@ -0,0 +1,18 @@
+-- Questao 6
+-- SELECT 
+--     Name AS 'Nome do Projeto', Hours AS 'Tempo de trabalho'
+-- FROM
+--     Projects;
+
+-- Questao 7
+-- SELECT 
+--     Name
+-- FROM
+--     Scientists
+-- ORDER BY Name;
+
+-- Questao 10
+SELECT *
+FROM
+    Projects
+ORDER BY Hours DESC LIMIT 3;
