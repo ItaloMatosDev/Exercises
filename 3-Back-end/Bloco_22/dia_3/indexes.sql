c9a1c30bc23da67c84140ed84d114b46a7916f11
Italo <italomatos764@gmail.com>
2021-08-02 14:35:03 -0400

    Bloco 22 dia 3

---
+++ 3-Back-end/Bloco_22/dia_3/indexes.sql
@@ -0,0 +1,11 @@
+CREATE INDEX index_first_name ON sakila.actor(first_name);
+
+SELECT *
+FROM sakila.actor
+WHERE first_name = 'RITA';
+
+DROP INDEX index_first_name ON sakila.actor;
+
+SELECT *
+FROM sakila.actor
+WHERE first_name = 'RITA';
